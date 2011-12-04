1dc3a04a744e912e3569538cb5494a00a81fd2c8
patrick <if10b047@technikum-wien.at>
2011-12-04 22:54:13 +0100

    navi added

---
+++ src/at/swe1ue4/plugins/NaviPlugin.java
@@ -0,0 +1,17 @@
+package at.swe1ue4.plugins;
+
+public class NaviPlugin implements PluginInterface {
+
+	@Override
+	public int rateString(String[] text) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getMessageForString(String[] text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
