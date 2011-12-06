be10a029b56de0bfbcd75a55169296d7b65a9536
patrick <if10b047@technikum-wien.at>
2011-12-06 10:00:20 +0100

    Osm SAX Parser

--- src/at/swe1ue4/pluginHelpers/OsmHandler.java
+++ src/at/swe1ue4/pluginHelpers/OsmHandler.java
@@ -1,7 +1,10 @@
 package at.swe1ue4.pluginHelpers;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Stack;
 
 import org.xml.sax.Attributes;
@@ -9,14 +12,16 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class OsmHandler extends DefaultHandler {
+	static Integer idCount = 0;
 	static Map<String, Integer> streetMap = new HashMap<String, Integer>();
-	static Map<Integer, String> cityMap = new HashMap<Integer, String>();
+	static Map<String, Integer> buildupMap = new HashMap<String, Integer>(); 
+	static Map<Integer, String> lookupMap = new HashMap<Integer, String>();
 	static Stack<String> elementStack = new Stack<String>();
 	
 	String getCityForStreet(String street) {
-		Integer cityId = streetMap.get( street );
+		Integer id = streetMap.get(street);
 		
-		return cityMap.get( cityId );
+		return lookupMap.get(id);
 	}
 	
 	@Override
@@ -26,7 +31,7 @@ public class OsmHandler extends DefaultHandler {
             Attributes attributes)
      throws SAXException {
 		if(qName.equals("tag") && elementStack.peek().equals("way")) {
-			;
+			streetMap.put(attributes.getValue("addr:street"), getIdForCity( attributes.getValue("addr:city") ));
 		}
 		
 		elementStack.push(qName);
@@ -36,5 +41,51 @@ public class OsmHandler extends DefaultHandler {
     public void endElement(String uri, String localName, String qName)
         throws SAXException {
       elementStack.pop();
-    }
+	}
+	
+	@Override
+	public void endDocument()
+            throws SAXException {
+		FileWriter f = null;
+		FileWriter f2 = null;
+		try {
+			f = new FileWriter("/Users/patrick/streetdump");
+			f2 = new FileWriter("/Users/patrick/citydump");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(Entry<String, Integer> entry : streetMap.entrySet()) {
+			try {
+				f.write(entry.getKey() + " " + entry.getValue());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		for(Entry<String, Integer> entry : buildupMap.entrySet()) {
+			try {
+				f2.write(entry.getKey() + " " + entry.getValue());
+				lookupMap.put(entry.getValue(), entry.getKey());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		buildupMap = new HashMap<String, Integer>();
+	}
+	
+	Integer getIdForCity(String city) {
+		Integer retVal = 0;
+		
+		if( (retVal = buildupMap.get(city)) != null) {
+			return retVal;
+		} else {
+			buildupMap.put(city, idCount);
+			return idCount++; 
+		}
+	}
 }
